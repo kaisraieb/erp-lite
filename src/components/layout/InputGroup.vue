417750e37632bf1e582ed952158f67ed4e13c046
Kais Sraieb <kaissraieb@gmail.com>
2026-07-28 20:49:22 +0100

    feat-auth: ajustement dans LoginView.vue [l'ajout de remember me et forget password]

--- src/components/layout/InputGroup.vue
+++ src/components/layout/InputGroup.vue
@@ -1,22 +1,29 @@
 <template>
-<div class="flex items-center border-2 rounded-lg px-2 py-3 gap-2 my-8 text-lg dark:bg-gray-800 bg-gray-300 border-gray-500 text-gray-500">
+  <div
+    class="flex items-center border-2 rounded-lg px-2 py-3 gap-2 my-8 text-lg dark:bg-gray-800 bg-gray-300 border-gray-500 text-gray-500"
+  >
     <label :for="'id-' + props.name">
-        <component :is="iconLabel" v-if="props.iconLabel" />
+      <component :is="iconLabel" v-if="props.iconLabel" />
     </label>
     <Input
-        :id="'id-' + props.name"
-        :type="inputType"
-        :name="props.name"
-        :placeholder="props.placeholder"
-        @input="props.onInput"
-        :title="props.hoverTitle"
-        :class="props.cssClasses"
+      :id="'id-' + props.name"
+      :type="inputType"
+      :name="props.name"
+      :placeholder="props.placeholder"
+      @input="props.onInput"
+      :title="props.hoverTitle"
+      :class="props.cssClasses"
     />
-    <button type="button" class="cursor-pointer" v-if="props.showHide && props.type === 'password'" @click="handleShowHide">
-        <Eye v-if="!isVisible"  />
-        <EyeSlash v-if="isVisible" />
+    <button
+      type="button"
+      class="cursor-pointer"
+      v-if="props.showHide && props.type === 'password'"
+      @click="handleShowHide"
+    >
+      <Eye v-if="!isVisible" />
+      <EyeSlash v-if="isVisible" />
     </button>
-</div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -24,22 +31,24 @@ import Input, { type InputPropsType } from "@/components/ui/Input.vue";
 import { Eye, EyeSlash } from "@boxicons/vue";
 import { computed, ref, type Component } from "vue";
 
-const props = defineProps<InputPropsType & {
-  showHide?: boolean
-  iconLabel?: Component
-}>()
+const props = defineProps<
+  InputPropsType & {
+    showHide?: boolean;
+    iconLabel?: Component;
+  }
+>();
 
-const isVisible = ref(false)
+const isVisible = ref(false);
 
 const inputType = computed(() => {
   if (props.type !== "password") {
-    return props.type ?? "text"
+    return props.type ?? "text";
   }
 
-  return isVisible.value ? "text" : "password"
-})
+  return isVisible.value ? "text" : "password";
+});
 
 const handleShowHide = () => {
-  isVisible.value = !isVisible.value
-}
+  isVisible.value = !isVisible.value;
+};
 </script>
